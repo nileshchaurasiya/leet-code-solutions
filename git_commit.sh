05ee8a19e80ca1014cd74a85efc5a95ece6c0b7f
Rohit <rohitchaursiya7383@gmail.com>
2026-07-17 17:31:47 +0530

    initial Test for sh command

--- git_commit.sh
+++ git_commit.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-message=initial Test for sh command
+message='initial Test for sh command'
+
+# echo "$message"
 
 git status
 
 git add .
 
-git commit -m '$message'
+git commit -m "$message"
 
 git push
+
+echo "Code push Success...\n commit message = $message"
