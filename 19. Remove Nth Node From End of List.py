6d88e0cdf110fb9bf725e60b480848fe5517a703
Rohit <chaurasiyanilesh22@gmail.com>
2026-07-20 18:09:38 +0530

    local user changes

---
+++ 19. Remove Nth Node From End of List.py
@@ -0,0 +1,18 @@
+# Given the head of a linked list, remove the nth node from the end of the list and return its head.
+# Input: head = [1,2,3,4,5], n = 2
+# Output: [1,2,3,5]
+# Example 2:
+
+# Input: head = [1], n = 1
+# Output: []
+# Example 3:
+
+# Input: head = [1,2], n = 1
+# Output: [1]
+
+#solution
+
+arr=[1,2,3,4,5]
+target=2
+a=len(arr)-target
+prinnt(a)
